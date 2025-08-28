8a7a5cf73a25e860e80292654b2a930e86714612
Atlanta <308220728@qq.com>
2025-08-28 09:40:25 +0800

    1.0 created

---
+++ src/main/java/org/example/mall/EMallApplication.java
@@ -0,0 +1,13 @@
+package org.example.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EMallApplication.class, args);
+    }
+
+}
