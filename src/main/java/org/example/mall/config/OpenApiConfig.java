cba4bb39b4b0fb8f2037e4620e66a93bf49fa79f
Atlanta <308220728@qq.com>
2025-08-28 10:41:06 +0800

    1.4 swagger

---
+++ src/main/java/org/example/mall/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package org.example.mall.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * OpenAPI配置类
+ * 用于配置Swagger文档的基本信息
+ * 
+ * @author Mall
+ * @version 1.3
+ */
+@Configuration
+public class OpenApiConfig {
+    
+    // SpringDoc会自动配置，这里可以添加自定义配置
+    // 等SpringDoc依赖下载完成后，我们可以添加更多配置
+    
+}
